17d2731ba15fd7dcfb6bee0ddba707239d9c37e0
Tom Hancocks <tomhancocks@gmail.com>
2019-09-01 19:13:18 +0100

    Further modifications to page management in MMU.

--- cpu/mmu.c
+++ cpu/mmu.c
@@ -59,7 +59,7 @@ void m68_mmu_destroy(void)
 			}
 
 			/* Fetch the page table */
-			union m68_mmu_page_entry *PAGE_TABLE = (void *)(MMU_PAGE_DIR[i].field.address << 4);
+			union m68_mmu_page_entry *PAGE_TABLE = (void *)((uintptr_t)MMU_PAGE_DIR[i].field.address << 4);
 
 			/* Iterate over all pages in the table */
 			for (int j = 0; j < MMU_PAGE_TABLE_MAX_ENTRIES; ++j) {
@@ -68,7 +68,7 @@ void m68_mmu_destroy(void)
 				}
 
 				/* Fetch the page */
-				void *PAGE = (void *)(PAGE_TABLE[i].field.address << 4);
+				void *PAGE = (void *)((uintptr_t)PAGE_TABLE[i].field.address << 4);
 				free(PAGE);
 			}
 
@@ -103,10 +103,10 @@ void *m68_mmu_page_alloc(uint32_t address)
 			table = (void *)address;
 		}
 
-		MMU_PAGE_DIR[dir_idx].field.address = (address >> 2) & ~0xF;
+		MMU_PAGE_DIR[dir_idx].field.address = (address >> 2);
 		MMU_PAGE_DIR[dir_idx].field.present = 1;
 	} else {
-		table = (void *)(MMU_PAGE_DIR[dir_idx].field.address << 2);
+		table = (void *)((uintptr_t)MMU_PAGE_DIR[dir_idx].field.address << 2);
 	}
 
 	/* Now check for the presence of the desired page in the table. If the page
@@ -119,10 +119,10 @@ void *m68_mmu_page_alloc(uint32_t address)
 			page = (void *)address;
 		}
 
-		table[table_idx].field.address = (address >> 2) & ~0xF;
+		table[table_idx].field.address = (address >> 2);
 		table[table_idx].field.present = 1;
 	} else {
-		page = (void *)(table[table_idx].field.address << 2);
+		page = (void *)((uintptr_t)table[table_idx].field.address << 2);
 	}
 
 	return page;
